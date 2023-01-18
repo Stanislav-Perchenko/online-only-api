c001ca8f78a1324667c30eaf9e08e84a1d68b736
Stanislav Perchenko <stanislav.perchenko@gmail.com>
2023-01-18 16:33:29 -0500

    REST API definitions

--- app/src/main/java/cam/alperez/samples/onlineonlyapi/rest/ApplicationRestService.java
+++ app/src/main/java/cam/alperez/samples/onlineonlyapi/rest/ApplicationRestService.java
@@ -1,16 +1,23 @@
 package cam.alperez.samples.onlineonlyapi.rest;
 
+import androidx.lifecycle.LiveData;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import cam.alperez.samples.onlineonlyapi.BuildConfig;
+import cam.alperez.samples.onlineonlyapi.entity.BookEntity;
+import cam.alperez.samples.onlineonlyapi.entity.CategoryEntity;
 import cam.alperez.samples.onlineonlyapi.rest.json.ApiGsonTypeAdapterFactory;
 import cam.alperez.samples.onlineonlyapi.rest.utils.LiveDataRetrofitCallAdapterFactory;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ApplicationRestService {
 
@@ -43,4 +50,10 @@ public interface ApplicationRestService {
             .addCallAdapterFactory(new LiveDataRetrofitCallAdapterFactory())
             .build()
             .create(ApplicationRestService.class);
+
+    @GET("/v3/f6a163bc-3e00-401e-aff2-e8a803ce79ba")
+    LiveData<List<CategoryEntity>> getCategories();
+
+    @GET("{api_link}")
+    LiveData<List<BookEntity>> getBooksForCategory(@Path("api_link") String apiLink);
 }
