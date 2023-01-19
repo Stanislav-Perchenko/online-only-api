38d6a5d760f1570878446cfdf3640d0c4300296d
Stanislav Perchenko <stanislav.perchenko@gmail.com>
2023-01-19 17:35:16 -0500

    Presentation layer of the category list

--- app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListActivity.java
+++ app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListActivity.java
@@ -2,22 +2,46 @@ package cam.alperez.samples.onlineonlyapi.ui.categorylist;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import cam.alperez.samples.onlineonlyapi.R;
 
 public class CategoryListActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout vRefresher;
+
     private CategoryListViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_refresh).setOnClickListener(v -> viewModel.fetchCategories());
+        setContentView(R.layout.activity_category_list);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setTitle(R.string.screen_title_category_list);
+
+        vRefresher = findViewById(R.id.refresher);
+
+        RecyclerView rv = findViewById(R.id.items_recycler);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        CategoryListAdapter listAdapter = new CategoryListAdapter(this);
+        listAdapter.setOnDataUpdateListener(() -> vRefresher.setRefreshing(false));
+        rv.setAdapter(listAdapter);
 
         viewModel = ViewModelProviders.of(this).get(CategoryListViewModel.class);
 
+        viewModel.getCategories().observe(this, listAdapter::setData);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        vRefresher.setOnRefreshListener(viewModel::fetchCategories);
     }
 }
---
+++ app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListAdapter.java
@@ -0,0 +1,88 @@
+package cam.alperez.samples.onlineonlyapi.ui.categorylist;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cam.alperez.samples.onlineonlyapi.R;
+import cam.alperez.samples.onlineonlyapi.entity.CategoryEntity;
+
+public class CategoryListAdapter extends RecyclerView.Adapter<CategoryListAdapter.ViewHolder> {
+
+    public interface OnDataUpdateListener {
+        void onDataUpdated();
+    }
+
+    private final LayoutInflater inflater;
+
+    private final List<CategoryEntity> data;
+
+    private OnDataUpdateListener onDataUpdateListener;
+
+    public CategoryListAdapter(Context ctx) {
+        inflater = LayoutInflater.from(ctx);
+        data = new ArrayList<>(10);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setData(List<CategoryEntity> data) {
+        this.data.clear();
+        this.data.addAll(data);
+        notifyDataSetChanged();
+        if (onDataUpdateListener != null) {
+            onDataUpdateListener.onDataUpdated();
+        }
+    }
+
+    public void setOnDataUpdateListener(OnDataUpdateListener l) {
+        this.onDataUpdateListener = l;
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return data.get(position).getId().idValue;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.category_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        CategoryEntity model = data.get(position);
+        holder.bindData(position, model);
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private int position;
+        private TextView tvTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.category_title);
+        }
+
+        void bindData(int position, CategoryEntity category) {
+            this.position = position;
+            tvTitle.setText(category.getDisplayName());
+        }
+    }
+}
