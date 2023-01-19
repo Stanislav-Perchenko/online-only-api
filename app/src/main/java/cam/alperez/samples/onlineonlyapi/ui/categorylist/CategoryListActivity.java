06c2d0ee23ef476e8775f629dd351be92b7a8ce5
Stanislav Perchenko <stanislav.perchenko@gmail.com>
2023-01-19 11:47:34 -0500

    Observe for logs in ViewModel

--- app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListActivity.java
+++ app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListActivity.java
@@ -1,21 +1,13 @@
 package cam.alperez.samples.onlineonlyapi.ui.categorylist;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
-import java.util.List;
-import java.util.Locale;
-
 import cam.alperez.samples.onlineonlyapi.R;
-import cam.alperez.samples.onlineonlyapi.entity.CategoryEntity;
-import cam.alperez.samples.onlineonlyapi.rest.utils.ApiResponse;
 
 public class CategoryListActivity extends AppCompatActivity {
-
-    public static final String TAG = "CategoryListActivity";
 
     private CategoryListViewModel viewModel;
 
@@ -27,29 +19,5 @@ public class CategoryListActivity extends AppCompatActivity {
 
         viewModel = ViewModelProviders.of(this).get(CategoryListViewModel.class);
 
-        viewModel.getCategoriesApiResponse().observe(this, (ApiResponse<List<CategoryEntity>> value) -> {
-            String text = String.format(Locale.UK,
-                    "Original API response: isSuccess=%b, httpCode=%d, httpMessage=%s, nItems=%s",
-                    value.isSuccessful(),
-                    value.httpCode(),
-                    value.httpMessage(),
-                    (value.getResponseData() == null) ? "null (0)" : ""+value.getResponseData().size());
-            Log.d(TAG, text);
-        });
-
-        viewModel.getCategories().observe(this, (List<CategoryEntity> value) -> {
-            Log.i(TAG, "Successful response: nItems = "+value.size());
-        });
-
-        viewModel.getCategoriesError().observe(this, (ApiResponse<List<CategoryEntity>> value) -> {
-            String text = String.format(Locale.UK,
-                    "API error response: isSuccess=%b, exception=%s, httpCode=%d, httpMessage=%s, nItems=%s",
-                    value.isSuccessful(),
-                    (value.getLocalError() == null) ? "null" : (value.getLocalError().getClass().getSimpleName() + " - " + value.getLocalError().getMessage()),
-                    value.httpCode(),
-                    value.httpMessage(),
-                    (value.getResponseData() == null) ? "null (0)" : ""+value.getResponseData().size());
-            Log.e(TAG, text);
-        });
     }
 }
--- app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListViewModel.java
+++ app/src/main/java/cam/alperez/samples/onlineonlyapi/ui/categorylist/CategoryListViewModel.java
@@ -1,13 +1,16 @@
 package cam.alperez.samples.onlineonlyapi.ui.categorylist;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.Transformations;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import cam.alperez.samples.onlineonlyapi.entity.CategoryEntity;
 import cam.alperez.samples.onlineonlyapi.rest.ApplicationRestService;
@@ -17,6 +20,8 @@ import cam.alperez.samples.onlineonlyapi.utils.ReplaceableSourceMediatorLiveData
 
 public class CategoryListViewModel extends AndroidViewModel {
 
+    public static final String TAG = "CategoryListViewModel";
+
     private final ReplaceableSourceMediatorLiveData<ApiResponse<List<CategoryEntity>>> categoriesResponseMediator;
 
     private final LiveData<List<CategoryEntity>> categories;
@@ -24,6 +29,7 @@ public class CategoryListViewModel extends AndroidViewModel {
 
     public CategoryListViewModel(Application app) {
         super(app);
+        Log.i("CategoryListActivity", "----> ViewModel create!");
         categoriesResponseMediator = new ReplaceableSourceMediatorLiveData<>();
 
         categories = Transformations.map(
@@ -39,7 +45,15 @@ public class CategoryListViewModel extends AndroidViewModel {
                 (ApiResponse<List<CategoryEntity>> input) -> input.isSuccessful() ? null : input
         ));
 
+        observeCategoriesLiveDataForLogs();
+
         fetchCategories();
+    }
+
+    private void observeCategoriesLiveDataForLogs() {
+        categoriesResponseMediator.observeForever(categoriesResponseMediatorObserver);
+        categories.observeForever(categoriesObserver);
+        categoriesError.observeForever(categoriesErrorObserver);
     }
 
     public LiveData<List<CategoryEntity>> getCategories() {
@@ -50,15 +64,48 @@ public class CategoryListViewModel extends AndroidViewModel {
         return categoriesError;
     }
 
-    public LiveData<ApiResponse<List<CategoryEntity>>> getCategoriesApiResponse() {
-        return categoriesResponseMediator;
-    }
-
     public void fetchCategories() {
+        Log.i("CategoryListActivity", "----> ViewModel enter fetch");
         LiveData<ApiResponse<List<CategoryEntity>>> result = ApplicationRestService
                 .INSTANCE.getCategories();
 
         categoriesResponseMediator.setSource(result);
+        Log.i("CategoryListActivity", "<---- ViewModel exit fetch");
     }
 
+
+    private final Observer<ApiResponse<List<CategoryEntity>>> categoriesResponseMediatorObserver = value -> {
+        String text = String.format(Locale.UK,
+                "Original API response: isSuccess=%b, httpCode=%d, httpMessage=%s, nItems=%s, exception=%s",
+                value.isSuccessful(),
+                value.httpCode(),
+                value.httpMessage(),
+                (value.getResponseData() == null) ? "null (0)" : ""+value.getResponseData().size(),
+                (value.getLocalError() == null) ? "null" : (value.getLocalError().getClass().getSimpleName() + " - " + value.getLocalError().getMessage()));
+        Log.d(TAG, text);
+    };
+
+    private final Observer<List<CategoryEntity>> categoriesObserver = value -> {
+        Log.i(TAG, "Successful response: nItems = "+value.size());
+    };
+
+    private final Observer<ApiResponse<List<CategoryEntity>>> categoriesErrorObserver = value -> {
+        String text = String.format(Locale.UK,
+                "API error response: isSuccess=%b, exception=%s, httpCode=%d, httpMessage=%s, nItems=%s",
+                value.isSuccessful(),
+                (value.getLocalError() == null) ? "null" : (value.getLocalError().getClass().getSimpleName() + " - " + value.getLocalError().getMessage()),
+                value.httpCode(),
+                value.httpMessage(),
+                (value.getResponseData() == null) ? "null (0)" : ""+value.getResponseData().size());
+        Log.e(TAG, text);
+    };
+
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        categoriesResponseMediator.removeObserver(categoriesResponseMediatorObserver);
+        categories.removeObserver(categoriesObserver);
+        categoriesError.removeObserver(categoriesErrorObserver);
+    }
 }
